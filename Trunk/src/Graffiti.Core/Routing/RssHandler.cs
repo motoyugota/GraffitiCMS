562b02d6e534e210f96617d84e244978f0cd4607
kevin_cp <SND\kevin_cp>
2010-02-01 01:53:19 +0000

    Improved URL routing - now has infrastructure to support n-level categories (other work still needed) and added event for plugins to add routes. Added Blog Extensions Plugin to new core plugins folder, updated it to be compatible with trunk bits & use routing Updated version of CookComputing XML-RPC library

--- Trunk/src/Graffiti.Core/Routing/RssHandler.cs
+++ Trunk/src/Graffiti.Core/Routing/RssHandler.cs
@@ -6,58 +6,57 @@ using DataBuddy;
 
 namespace Graffiti.Core
 {
-    public class RssHandler : IRouteHandler
-    {
-        public IHttpHandler GetHttpHandler(RequestContext requestContext)
-        {
-            string categoryOneName = requestContext.RouteData.Values["categoryone"] != null ? requestContext.RouteData.Values["categoryone"].ToString() : null;
-            string categoryTwoName = requestContext.RouteData.Values["categorytwo"] != null ? requestContext.RouteData.Values["categorytwo"].ToString() : null;
+	public class RssHandler : IRouteHandler
+	{
+		public IHttpHandler GetHttpHandler(RequestContext requestContext)
+		{
+			string categoryOneName = requestContext.RouteData.Values["categoryone"] != null ? requestContext.RouteData.Values["categoryone"].ToString() : null;
+			string categoryTwoName = requestContext.RouteData.Values["categorytwo"] != null ? requestContext.RouteData.Values["categorytwo"].ToString() : null;
+			string tagName = requestContext.RouteData.Values["tagname"] != null ? requestContext.RouteData.Values["tagname"].ToString() : null;
 
-            string tagName = requestContext.RouteData.Values["tagname"] != null ? requestContext.RouteData.Values["tagname"].ToString() : null;
 
+			if (!String.IsNullOrEmpty(categoryTwoName))
+			{
+				return GetFeed(categoryTwoName);
+			}
 
-            if (!String.IsNullOrEmpty(categoryTwoName))
-            {
-                return GetFeed(categoryTwoName);
-            }
-            
-            if(!String.IsNullOrEmpty(categoryOneName))
-            {
-                return GetFeed(categoryOneName);
-            }
+			if (!String.IsNullOrEmpty(categoryOneName))
+			{
+				return GetFeed(categoryOneName);
+			}
 
-            if (!string.IsNullOrEmpty(tagName))
-            {
-                return GetTagFeed(tagName);
-            }
+			if (!string.IsNullOrEmpty(tagName))
+			{
+				return GetTagFeed(tagName);
+			}
 
-            return GetFeed(null);
-        }
+			return GetFeed(null);
+		}
 
-        public RSS GetTagFeed(string tagName)
-        {
-            var feed = new RSS();
-            if (!string.IsNullOrEmpty(tagName))
-            {
-                feed.TagName = tagName;
-            }
-            return feed;
-        }
+		public RSS GetTagFeed(string tagName)
+		{
+			var feed = new RSS();
+			if (!string.IsNullOrEmpty(tagName))
+			{
+				feed.TagName = tagName;
+			}
+			return feed;
+		}
 
-        public RSS GetFeed(string categoryName)
-        {
-            var feed = new RSS();
+		public RSS GetFeed(string categoryName)
+		{
+			var feed = new RSS();
 
-            if (!String.IsNullOrEmpty(categoryName))
-            {
-                var category = new CategoryController().GetCachedCategoryByLinkName(categoryName, false);
+			if (!String.IsNullOrEmpty(categoryName))
+			{
+				var category = new CategoryController().GetCachedCategoryByLinkName(categoryName, false);
 
-                feed.CategoryID = category.Id;
-                feed.CategoryName = category.LinkName;
-            }
+				feed.CategoryID = category.Id;
+				feed.CategoryName = category.LinkName;
+			}
 
-            return feed;
-        }
+			return feed;
+		}
 
-    }
+	}
 }
