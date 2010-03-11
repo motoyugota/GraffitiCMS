6b9319b2cdf465673b792cb36ec2333cf96c1355
kevin_cp <SND\kevin_cp>
2010-03-11 23:15:04 +0000

    * Updated CKEditor component to stable 3.2 version (was prev a nightly build of 3.1) * Fixed issue #12010 - was a bug relating to how File Selector callback was being handled. It needed to be different based when whether called by the CKeditor or a standard Graffiti page * Fixed a related js issue on FileFormElement & User Management page found & fixed by user jillebrew (this checkin is for the 1.3 branch, trunk will be synced up soon)

--- src/Graffiti.Core/Controls/FileBrowser.cs
+++ src/Graffiti.Core/Controls/FileBrowser.cs
@@ -29,27 +29,6 @@ namespace Graffiti.Core
 
         protected override void Render(HtmlTextWriter writer)
         {
-            
-            //StringBuilder sb = new StringBuilder();
-            //sb.Append("var fileBrowserCallback = null;\n");
-            //sb.Append("function OpenFileBrowser(callbackFunction, fileType){\n");
-            //sb.Append("fileBrowserCallback = callbackFunction;\n");
-            //sb.AppendFormat("var url = '{0}';\n", VirtualPathUtility.ToAbsolute("~/__utility/Telligent_Editor/editor/filemanager/browser/default/browser.html"));
-            //sb.AppendFormat("url += '?Connector=' + encodeURIComponent('{0}');\n",VirtualPathUtility.ToAbsolute("~/__utility/Telligent_Editor/editor/filemanager/browser/default/connectors/aspx/connector.aspx"));
-
-            //sb.AppendFormat("url += '&ServerPath=' + encodeURIComponent('{0}');\n", Context.Application["Telligent_Glow_Editor:UserFilesPat"]);
-            //sb.Append("url += '&Type=' + fileType;\n");
-            //sb.Append("window.open(url, 'TEBrowseWindow', \"toolbar=no,status=no,resizable=yes,dependent=yes,scrollbars=yes,width=\" + (screen.width * 0.7) + \",height=\" + (screen.height * 0.7) + \",left=\" + ((screen.width * 0.3) / 2) + \",top=\" + ((screen.height * 0.3) / 2));\n");
-            //sb.Append("}\n");
-
-            //sb.Append("function SetUrl(url)\n");
-            //sb.Append("{\n");
-            //sb.Append("      var host = window.location.toString().substr(0, window.location.toString().indexOf('/', 8));\n");
-            //sb.Append("      fileBrowserCallback(host + url);\n");
-            //sb.Append("}\n");
-
-            //this.Page.ClientScript.RegisterClientScriptBlock(GetType(),"FileBrowser",sb.ToString(),true);
-
             RegisterJavaScript(this.Page);
 
             base.Render(writer);
